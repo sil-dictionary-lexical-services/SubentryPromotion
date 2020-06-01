baa896bde07db1557248b03816ab67bd65d5b355
Wes <wes_peacock@sil.org>
2020-06-01 08:11:53 -0600

    Add a bash wrapper script to do multiple runs of se2lx.pl

---
+++ se2lx/runse2lx.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# The next 3 variables should be set before the script is run
+# if they're not, a default is used
+dbname="${dbname:-BearBullMole-complex.db}"
+semarkers="${semarkers:-lc ld li lp selc selp}"
+inifile="${inifile:-Sample-lc.ini}"
+# promodbname is set to dbname with '-promo' inserted into if before the extension
+promodbname="${dbname%.*}" # take off extension
+if [ $promodbname == "${dbname##*.}" ]; then # no extension
+	promodbname="$promodbname-promo"
+else
+	promodbname="$promodbname-promo.${dbname##*.}"
+fi
+# a couple of temporary filenames
+from=$(mktemp)
+to=$(mktemp)
+cp "$dbname"  $from
+for sfm in $semarkers
+do
+	echo "Promoting $sfm fields"
+	./se2lx.pl --section se2lx_$sfm  --inifile $inifile <$from >$to
+	cp $to $from
+done
+cp $to "$promodbname"
+rm $from $to
