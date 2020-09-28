4352b783041d249efaf4f1a43dbd17a0118e4624
Wes <wes_peacock@sil.org>
2020-09-27 19:11:18 -0600

    Allow runVar2Compform.sh to be sourced
     * $0 might not contain a script name
     * don't use any exit commands if sourced
     * indent filenames on error

--- Var2Compform/runVar2Compform.sh
+++ Var2Compform/runVar2Compform.sh
@@ -3,9 +3,12 @@
 # ignore the initial backup file
 ignore="${ignore:-BearBullMole-Initial-Empty.fwbackup}"
 if [ $(ls  -1 *.{fwbackup,zip} 2>/dev/null |grep -v $ignore |wc -l) != 1 ] ; then
-	echo >&2  $(basename $0) "requires exactly one fwbackup/zip file in this directory"
+	echo >&2   "This script requires exactly one fwbackup/zip file in this directory"
 	echo >&2 "It found:"
-	ls  -1 *.{fwbackup,zip} 2>/dev/null | grep -v $ignore
+	ls  -1 *.{fwbackup,zip} 2>/dev/null | grep -v $ignore | sed -e 's/^/     /'
+	if [ "$0" != "$BASH_SOURCE" ]; then # if the file is sourced, don't just exit
+		echo "Ctrl-C to break, Enter to leave WSL"; read
+		fi
 	exit
 fi
 
@@ -22,5 +25,4 @@ mv "$fwdatafile" "$barefname-before.fwdata"
 mv PromoteSubentries.ini  PromoteSubentries.bak
 perl -pE "s/FwdataIn.*/FwdataIn=$barefname-before.fwdata/; s/FwdataOut.*/FwdataOut=$fwdatafile/" PromoteSubentries.bak > PromoteSubentries.ini
 ./Var2Compform.pl
-zip "$backupfile" "$fwdatafile"  # 2>/dev/null 
-exit
+zip "$backupfile" "$fwdatafile"  # 2>/dev/null
